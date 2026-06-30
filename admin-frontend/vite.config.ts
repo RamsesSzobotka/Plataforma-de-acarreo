ec9bfe29ba831de08d10984a3b501f56c0fff1b6
RamsesSzobotka <vlastoskr@gmail.com>
2026-06-30 17:37:43 -0500

    fix. arreglar ocnexiones en deploy

--- admin-frontend/vite.config.ts
+++ admin-frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     port: 5174,
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://cargly-backend.onrender.com',
         changeOrigin: true,
       },
     },
